5e9ec2b03128d387e6b49ae5de6a1f9f10616e1d
Shivansh Jagga <shivansh.jagga@gmail.com>
2025-06-20 17:58:49 -0400

    small edit

--- src/config/settings.py
+++ src/config/settings.py
@@ -7,7 +7,7 @@ import os
 @dataclass
 class AppSettings:
     model: str = field(default='claude-3-7-sonnet-20250219')
-    api_key: str = field(default='')
+    api_key: str = field(default=' ')
     api_base: Optional[str] = field(default='https://api.anthropic.com/api/v1')
     temperature: float = field(default=float(0.2))
     max_tokens: int = field(default=int(os.getenv("LLM_MAX_TOKENS", 6400)))
